33479a00063bdd3262733d51c5c9568a422ded50
Rizky Dharma <rizkydarma536@gmail.com>
2024-07-11 11:55:45 +0700

    nothing

--- app/view/home/index.php
+++ app/view/home/index.php
@@ -3,7 +3,7 @@
         <h1 class="display-4">Selamat Datang di Perpustakaan Malang</h1>
         <p class="lead">Kamu dapat melihat buku yang tersedia dan peminjamnya di Website Perpustakaan Malang</p>
         <hr class="my-4">
-        <p>Lihat kolek si buku kita dan mulai lah membaca.</p>
+        <p>Lihat koleksi buku kita dan mulai lah membaca.</p>
         <p class="lead">
             <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
         </p>
