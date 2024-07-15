50713ba2cc22e98e633bf9be4a8cd01b2526e2e2
Rizky Dharma <rizkydarma536@gmail.com>
2024-07-15 13:34:33 +0700

    fitur detail sudah ditambahkan tetapi masih belum bisa kembali ke page buku

--- app/view/buku/detail.php
+++ app/view/buku/detail.php
@@ -1,13 +1,18 @@
 
-<div class="container">
-    <div class="card" style="width: 18rem;">
+<div class="container mt-5">
+    <div class="card">
         <div class="card-body">
             <h5 class="card-title"><?= $data['buku']['judul']?></h5>
-            <h6 class="card-subtitle mb-2 text-body-secondary"><?= $data['buku']['kode']?></h6>
+            <h6 class="card-subtitle mb-2 text-body-secondary"><?= $data['buku']['kode_buku']?></h6>
             <p class="card-text"><?= $data['buku']['pengarang']?></p>
             <p class="card-text"><?= $data['buku']['penerbit']?></p>
-            <p class="card-text"><?= $data['buku']['tahun']?></p>
-            <a href="<?= BASEURL; ?>/buku" class="card-link">Back</a>
+            <p class="card-text"><?= $data['buku']['tahun_terbit']?></p>
+            <p class="card-text"> buku ini berjudul <?= $data['buku']['judul']?> , dikarang oleh <?= $data['buku']['pengarang']?> 
+                dan diterbitkan oleh <?= $data['buku']['penerbit']?> pada tahun <?= $data['buku']['tahun_terbit']?>, sekian informasi 
+                tentang buku tersebut, untuk sinopsis masih tidak tersedia :)</p>
+            <form action="<?= URLUTAMA;?>/buku" method="post">
+                <button class="btn btn-danger" type="button">Back</button>
+            </form>
         </div>
     </div>
 </div>
