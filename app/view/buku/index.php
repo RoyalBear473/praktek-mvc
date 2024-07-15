bc001ec240e9efa532e6f15ffc34b2060c84836d
Rizky Dharma <rizkydarma536@gmail.com>
2024-07-15 16:16:37 +0700

    nothing

--- app/view/buku/index.php
+++ app/view/buku/index.php
@@ -63,10 +63,10 @@
                                 <button class="btn btn-danger" onclick="return confirm('ingin menghapus buku ini?')">Delete</button>
                             </form>
                             <form action="<?= URLUTAMA; ?>/buku/detail/<?= $buku['id'];?>" method="post">
-                                <button class="btn btn-secondary" id="updateBuku"  data-bs-toggle="modal" data-bs-target="#formModal">Detail</button>
+                                <button class="btn btn-secondary">Detail</button>
                             </form>
                             <form action="<?= URLUTAMA; ?>/buku/update/<?= $buku['id'];?>" method="post">
-                                <button class="btn btn-primary" id="updateBuku"  data-bs-toggle="modal" data-bs-target="#formModal">Update</button>
+                                <button class="btn btn-primary" id="updateBuku"  data-bs-toggle="modal" data-bs-target="#formModal" data-id="<?=$buku['id'];?>">Update</button>
                             </form>
                         </div>
                     </div>
